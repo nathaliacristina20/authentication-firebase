774031a3163ac60f063c33522e855e65e3c20a1b
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-07-21 21:21:19 -0300

    feature: add config of fireBase in enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: 'AIzaSyBrcFSpxqH7FxtoDbnOeaFix1cTKuDXAZo',
+        authDomain: 'authentication-angular-3b550.firebaseapp.com',
+        databaseURL: 'https://authentication-angular-3b550.firebaseio.com',
+        projectId: 'authentication-angular-3b550',
+        storageBucket: 'authentication-angular-3b550.appspot.com',
+        messagingSenderId: '436201466776',
+        appId: '1:436201466776:web:87fae4720f8cb3bd4e19ed',
+        measurementId: 'G-DH671GLDHB'
+    }
 };
